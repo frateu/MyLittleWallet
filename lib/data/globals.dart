4528a16e192c751dff7b89f0a5910b6e58e6722c
frateu <matheus.fanjos@outlook.com>
2022-09-27 20:46:58 -0300

    Novas logicas dos dados

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -1,5 +1,15 @@
 library globals;
 
+class Lancamento {
+  String desc;
+  double value;
+
+  Lancamento({required this.desc, required this.value});
+}
+
 double allMoney    = 0;
 double allReceita  = 0;
 double allDespesa  = 0;
+
+List<Lancamento> lancamentosReceita = [];
+List<Lancamento> lancamentosDespesa = [];
--- lib/functions/money.dart
+++ lib/functions/money.dart
@@ -1,19 +1,23 @@
 import 'package:mylittlewallet/data/globals.dart' as globals;
 
-void addMoney(String value){
+void addMoney(String descMoney, String valueMoney){
 
-  double money = double.parse(value);
+  double money = double.parse(valueMoney);
 
   globals.allMoney = globals.allMoney + money;
   globals.allReceita = globals.allReceita + money;
 
+  globals.lancamentosReceita.add(globals.Lancamento(desc: descMoney, value: money));
+
 }
 
-void removeMoney(String value){
+void removeMoney(String descMoney, String valueMoney){
 
-  double money = double.parse(value);
+  double money = double.parse(valueMoney);
 
   globals.allMoney = globals.allMoney - money;
   globals.allDespesa = globals.allDespesa + money;
+
+  globals.lancamentosDespesa.add(globals.Lancamento(desc: descMoney, value: money));
 
 }
